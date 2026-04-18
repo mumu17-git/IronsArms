1bff1e38882158f77d0245a0dfae8a2c0bb9f134
mumu17-git <moonpapa1709@gmail.com>
2026-04-18 21:12:40 +0900

    Update 3.0.5

--- src/main/java/com/mumu17/ironsarms/IronsArms.java
+++ src/main/java/com/mumu17/ironsarms/IronsArms.java
@@ -8,6 +8,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.ModLoadingException;
+import net.minecraftforge.fml.ModLoadingStage;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,6 +25,17 @@ public class IronsArms {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public IronsArms() {
+        if (ModList.get().isLoaded("arscurios")
+                || ModList.get().isLoaded("armslib")
+                || ModList.get().isLoaded("castlib")) {
+            throw new ModLoadingException(
+                    ModLoadingContext.get().getActiveContainer().getModInfo(),
+                    ModLoadingStage.CONSTRUCT,
+                    "This mod is incompatible with §eArsCurios, ArmsLib and CastLib§r. Please remove them",
+                    new Throwable()
+            );
+        }
+
         MinecraftForge.EVENT_BUS.register(IronsArmsBulletEvents.class);
         ModNetworking.register();
     }
